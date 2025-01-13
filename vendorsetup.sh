cace1bcbdc8a5a28dda083bb46364af89f031547
Nomishaw21 <Nomishaw9@example.com>
2025-01-13 17:46:43 +0530

    stone: bring up flare

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/Nomishaw21/device_xiaomi_stone-kernel device/xiaomi/stone-kernel
+git clone https://github.com/Nomishaw21/vendor_xiaomi_stone vendor/xiaomi/stone --depth=1
+
