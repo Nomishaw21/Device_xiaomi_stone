760daefebc99a3449df401c2953e7822e7b6a1bd
Nomishaw21 <Nomishaw9@example.com>
2025-01-13 17:46:43 +0530

    stone: bring up flare

--- flare_stone.mk
+++ flare_stone.mk
@@ -9,14 +9,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/flare/config/common_full_phone.mk)
 
 # Inherit from stone device
 $(call inherit-product, device/xiaomi/stone/device.mk)
 
 PREBUILT_KERNEL := true
 
-PRODUCT_NAME := lineage_stone
+PRODUCT_NAME := flare_stone
 PRODUCT_DEVICE := stone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
@@ -24,3 +24,10 @@ PRODUCT_BRAND := Xiaomi
 BuildFingerprint=POCO/moonstone_p_global/moonstone:14/UKQ1.231003.002/V816.0.10.0.UMPMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Flare flags..
+FLARE_BUILD_TYPE := OFFICIAL
+FLARE_MAINTAINER := Nomi..shaw
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+WITH_GAPPS := true
---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/nomishaw21/device_xiaomi_stone-kernel device/xiaomi/stone-kernel
+git clone https://github.com/kamikaonashi/vendor_xiaomi_stone vendor/xiaomi/stone --depth=1
+
