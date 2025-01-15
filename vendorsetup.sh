715aeeb89d33c9e3642819e9042733e5f155c964
Nomishaw21 <Nomishaw9@example.com>
2025-01-15 14:01:26 +0530

    stone: Fix qti header

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,6 @@
 git clone https://github.com/nomishaw21/device_xiaomi_stone-kernel device/xiaomi/stone-kernel
 git clone https://github.com/kamikaonashi/vendor_xiaomi_stone vendor/xiaomi/stone --depth=1
 
+# Rename conflicting qti_kernel_headers in source
+sed -i 's/"qti_kernel_headers"/"qti_kernel_headers_old"/g' vendor/flare/build/soong/Android.bp
+
